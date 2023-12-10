89f21290f17d6e3f9623a8779952879cbb0e567c
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-10 11:25:00 +0530

    Adding context into the django stuff

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Blog Post Definitions
+posts = [
+    {
+        "title": "The Art of Effective Communication",
+        "author": "John Smith",
+        "content": "Mastering communication skills for success in personal and professional relationships.",
+        "date": "2023-03-15"
+    },
+    {
+        "title": "Mastering Time Management Techniques",
+        "author": "Jane Doe",
+        "content": "Boost productivity with proven time management strategies for ultimate efficiency.",
+        "date": "2023-04-22"
+    },
+    {
+        "title": "Navigating the World of Remote Work",
+        "author": "Chris Johnson",
+        "content": "Adapt to challenges and thrive in the era of remote work.",
+        "date": "2023-05-10"
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1> Blog About</h1>')
+    return render(request, 'blog/about.html')
