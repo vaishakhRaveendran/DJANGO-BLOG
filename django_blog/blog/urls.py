959b4cf8b219681e120d0041da21d760ca76a9b9
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-15 12:29:43 +0530

    updated urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.home,name='blog-home'),
-    path("about/", views.about,name='blog-about'),
+    path("", views.home, name='blog-home'),
+    path("about/", views.about, name='blog-about'),
 ]
