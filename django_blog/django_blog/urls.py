a43c6c9fde0b168e07c9182bf51183d770295130
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-16 23:12:36 +0530

    rerouting for login and logout

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('blog.urls')),
     #chop off everything upto blog/ and send the remaining portions to blog.urls
-    path("login/",auth_views.LoginView.as_view,name='login'),
-    path("login/",auth_views.LogoutView.as_view,name='logout'),
+    path("login/",auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path("login/",auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path("register/",user_views.register,name='register'),
 ]
