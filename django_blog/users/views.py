fbc3249019b699c25f337691be6609ec14fdded6
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-17 11:19:54 +0530

    added user view with login_requred decorator

--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegisterationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
 def register(request):
@@ -9,9 +10,12 @@ def register(request):
             form.save()
             #using this method the user will be added and you can see it from the admin page.
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request,f'Account created for {username}. Login and amuzee!!!')
+            return redirect('login')
     else:
         form = UserRegisterationForm()
     return render(request, 'users/register.html', {'form': form})
 
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
