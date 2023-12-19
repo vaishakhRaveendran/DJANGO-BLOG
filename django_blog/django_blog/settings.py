8d34e9ffaddf6652dcf02f1b933be5d06a691c2f
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-19 22:13:17 +0530

    changing directory of profle pics

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -121,3 +121,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL='login'
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+MEDIA_URL ='/media/'
