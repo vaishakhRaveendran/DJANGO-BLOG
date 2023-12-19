85215b8cb93d247de8352559bb5ec8c824a2cbe6
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-19 22:31:29 +0530

    adding urls for profile page

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -28,3 +30,6 @@ urlpatterns = [
     path("register/",user_views.register,name='register'),
     path("profile/",user_views.profile,name="profile")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
